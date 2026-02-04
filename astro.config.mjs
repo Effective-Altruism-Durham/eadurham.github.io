e3570e0e127496dba91316c851918506b6c0aa28
Theosdoor <theo.farrell99@outlook.com>
2026-02-04 20:11:50 +0000

    Update site URL in astro.config.mjs to effective-altruism-durham.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://durhamaisafety.github.io',
+  site: 'https://effective-altruism-durham.github.io',
   integrations: [tailwind(), sitemap()],
   output: 'static',
 });
